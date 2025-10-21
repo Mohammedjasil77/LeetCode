6ef399b606bedd5495c4f9c837572bf5352545ee
Mohammedjasil77 <jasilchemmala@gmail.com>
2025-10-21 19:18:52 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (23.62%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 var numberOfSteps = function(num) {
-    let count = 0;
-    for( ; num>0 ; count++ ){
-    if(num % 2 === 0 ){
+    let steps = 0;
+    while( num > 0){
+    if( num  % 2 === 0){
         num = num / 2
-
-    }else{
-        num = num - 1
+    } else {
+        num  = num - 1
     }
+    steps++;
     }
-    return count;
-
+    return steps;
 };
